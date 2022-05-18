9023fec6b1b5ebcf03580d170d85f83d139de94d
Luís Fernandes <hsantos.luis@outlook.com>
2022-05-17 22:48:00 -0300

    Iniciando o projeto

---
+++ GestorClienteCMD/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestorClienteCMD
+{
+    class Program
+    {
+        [System.Serializable]
+        struct Cliente
+        {
+            public string nome;
+            public string email;
+            public string cpf;
+        }
+
+        static List<Cliente> clientes = new List<Cliente>();
+
+        enum Menu { Listagem = 1, Adicionar = 2, Remover = 3, Sair = 4 }
+
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+
+            while(!escolheuSair)
+            { 
+                Console.WriteLine("Sistema de Clientes - Bem Vindo");
+                Console.WriteLine("1-Listagem \n2-Adicionar \n3-Remover \n4-Sair");
+
+                int intOp = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intOp;
+
+                switch (opcao)
+                {
+                    case Menu.Adicionar:
+                        Adicionar();
+                        break;
+                    case Menu.Listagem:
+                        break;
+                    case Menu.Remover:
+                        break;
+                    case Menu.Sair:
+                        escolheuSair = true;
+                        break;
+                }
+
+                Console.Clear();
+            }
+        }
+
+        static void Adicionar()
+        {
+            Cliente cliente = new Cliente();
+            Console.WriteLine("Cadastro de cliente");
+
+            Console.WriteLine("Nome do cliente: ");
+            cliente.nome = Console.ReadLine();
+
+            Console.WriteLine("E-mai do cliente: ");
+            cliente.email = Console.ReadLine();
+
+            Console.WriteLine("CPF do cliente: ");
+            cliente.cpf = Console.ReadLine();
+
+            clientes.Add(cliente);
+
+            Console.WriteLine("Cadastro concluído, pressione ENTER para sair");
+            Console.ReadLine();
+
+        }
+    }
+}
